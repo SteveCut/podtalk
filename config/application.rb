90c119a031d370598b5e73ad859e98a9d22994df
Steve Cut <gstevecut@gmail.com>
2013-09-16 12:23:06 +0300

    Finish user edit, update, index, and destroy actions 8

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-if defined?(Bundler)
-# If you precompile assets before deploying to production, use this line
-  #Bundler.require(*Rails.groups(:assets => %w(development test)))
-# If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
-end
-
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
